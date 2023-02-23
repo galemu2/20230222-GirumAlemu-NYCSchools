a5221b4add04d12d157ef31a30c7b40cad5b1ca4
Girum A <galemu00@gmail.com>
2023-02-23 14:45:57 -0600

    Implement back navigation from scores to school list.

--- app/src/main/java/com/example/a20230222_girumalemu_nycschools/ui/MainActivity.kt
+++ app/src/main/java/com/example/a20230222_girumalemu_nycschools/ui/MainActivity.kt
@@ -1,12 +1,38 @@
 package com.example.a20230222_girumalemu_nycschools.ui
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.example.a20230222_girumalemu_nycschools.R
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import  com.example.a20230222_girumalemu_nycschools.R
+import com.example.a20230222_girumalemu_nycschools.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(binding.fragmentContainerView.id) as NavHostFragment
+        navController = navHostFragment.navController
+        binding.navigationView.setupWithNavController(navController)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
