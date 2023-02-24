44ca46ef48c498315406c268fe347ff364a245ed
Girum A <galemu00@gmail.com>
2023-02-23 19:59:19 -0600

    Minor fixes.

--- app/src/test/java/com/example/a20230222_girumalemu_nycschools/ui/viewModels/SchoolsViewModelTest.kt
+++ app/src/test/java/com/example/a20230222_girumalemu_nycschools/ui/viewModels/SchoolsViewModelTest.kt
@@ -1,30 +1,22 @@
 package com.example.a20230222_girumalemu_nycschools.ui.viewModels
 
 import android.content.Context
-import android.util.Log
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.a20230222_girumalemu_nycschools.data.DummySchoolsRepository
-import com.example.a20230222_girumalemu_nycschools.data.model.SatResult
-import com.example.a20230222_girumalemu_nycschools.getOrAwaitValue
 import com.example.a20230222_girumalemu_nycschools.util.Resource
 import com.google.common.truth.Truth
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.test.UnconfinedTestDispatcher
-import kotlinx.coroutines.test.resetMain
-import kotlinx.coroutines.test.runTest
-import kotlinx.coroutines.test.setMain
-import org.junit.Assert.*
-
+import kotlinx.coroutines.test.*
 import org.junit.After
+import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.junit.runners.JUnit4
 
 @OptIn(ExperimentalCoroutinesApi::class)
 @RunWith(AndroidJUnit4::class)
@@ -56,14 +48,19 @@ class SchoolsViewModelTest {
     @Test
     fun testGetSatResults(): Unit = runBlocking{
         // check for loading
-        val satResult = viewModel.satResult.value
          viewModel.getSatResults("123")
-         if(satResult is Resource.Success ) {
+        val satResult = viewModel.satResult.value
+
+        if(satResult is Resource.Success ) {
             val successResult = satResult.data?.get(0)
             successResult?.let {
                  Truth.assertThat(it.dbn).isEqualTo("123")
             }
-        }else{
+        }else if(satResult is Resource.Loading){
+            throw Exception("Stuck in Loading State")
+         }else if(satResult is Resource.Error){
+             throw Exception("Result is error")
+         } else{
             throw Exception("Test Failed")
          }
 
